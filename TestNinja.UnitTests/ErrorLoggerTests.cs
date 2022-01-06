e7e3ef667a93ea0678516a8530e1944fee0a95ec
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-06 20:37:46 +0530

    Testing Methods that throw Exceptions

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests;
+
+[TestFixture]
+public class ErrorLoggerTests
+{
+    private ErrorLogger _logger;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _logger = new();
+    }
+
+    [Test]
+    public void Log_WhenCalled_SetTheLastErrorProperty()
+    {
+        _logger.Log("a");
+        Assert.That(_logger.LastError, Is.EqualTo("a"));
+    }
+
+    [Test]
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase(" ")]
+    public void Log_InvalidError_ThrowArgumentNullException(string error)
+    {
+        // _logger.Log(error);
+        Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+        // Assert.That(() => _logger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>());
+    }
+}
