d8a8d0b6541013627a353de0e0733147fa4033a4
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-06 20:35:01 +0530

    Testing Void Methods

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests;
+
+[TestFixture]
+public class CustomerControllerTests
+{
+    private CustomerController _controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _controller = new CustomerController();
+    }
+
+    [Test]
+    public void GetCustomer_IdIsZero_ReturnNotFound()
+    {
+        var result = _controller.GetCustomer(0);
+
+        //NotFound Object
+        Assert.That(result, Is.TypeOf<NotFound>());
+        
+        //NotFound Object or one of its derivatives
+        // Assert.That(result, Is.InstanceOf<NotFound>());
+    }
+
+    [Test]
+    public void GetCustomer_IdIsNotZero_ReturnOk()
+    {
+        var result = _controller.GetCustomer(3);
+
+        //NotFound Object
+        Assert.That(result, Is.TypeOf<Ok>());
+    }
+}
