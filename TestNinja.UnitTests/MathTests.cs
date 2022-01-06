e32d32c105f53c80248433891e47bb518a68cf8e
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-06 20:34:43 +0530

    testing Arrays and Collection

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests;
@@ -7,13 +8,13 @@ namespace TestNinja.UnitTests;
 public class MathTests
 {
     private Math _math;
-    
+
     [SetUp]
     public void SetUp()
     {
         _math = new Math();
     }
-    
+
     [Test]
     [Ignore("Because I wanted to!")]
     public void Add_WhenCalled_ReturnSumOfArguments()
@@ -24,14 +25,33 @@ public class MathTests
     }
 
     [Test]
-    [TestCase(2,1,2)]
-    [TestCase(1,2,2)]
-    [TestCase(1,1,1)]
+    [TestCase(2, 1, 2)]
+    [TestCase(1, 2, 2)]
+    [TestCase(1, 1, 1)]
     public void Max_WhenCalled_ReturnGreaterArgument(int a, int b, int expectedResult)
     {
         var result = _math.Max(a, b);
 
         Assert.That(result, Is.EqualTo(expectedResult));
     }
-    
+
+    [Test]
+    [TestCase(5)]
+    public void GetOddNumbers_GivenLimitGreaterThanZero_ReturnOddNumbersUptoLimit(int limit)
+    {
+        var result = _math.GetOddNumbers(limit);
+
+        // Assert.That(result, Is.Not.Empty);
+
+        // Assert.That(result.Count(),Is.EqualTo(3));
+
+        // Assert.That(result, Does.Contain(1));
+        // Assert.That(result, Does.Contain(3));
+        // Assert.That(result, Does.Contain(5));
+
+        Assert.That(result, Is.EquivalentTo(new int[] {1, 3, 5}));
+
+        Assert.That(result, Is.Ordered);
+        Assert.That(result, Is.Unique);
+    }
 }
