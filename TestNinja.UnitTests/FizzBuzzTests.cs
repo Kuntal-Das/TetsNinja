620cb38e25494aefdc6610295fb84c671e967a52
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-07 12:49:19 +0530

    Ex1: FizzBuzz Tests

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    [Test]
+    [TestCase(19,"19")]
+    [TestCase(3,"Fizz")]
+    [TestCase(5,"Buzz")]
+    [TestCase(15,"FizzBuzz")]
+    public void GetOutput_WhenCalled_ReturnsString(int number, string expectedResult)
+    {
+        var result = FizzBuzz.GetOutput(number);
+        
+        Assert.That(result, Is.EqualTo(expectedResult).IgnoreCase);
+    }
+}
