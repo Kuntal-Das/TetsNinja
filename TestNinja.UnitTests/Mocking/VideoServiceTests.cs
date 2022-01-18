0e2dbd5bed87371d197f8a9ef1f63b7563ee2a0f
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-18 19:30:29 +0530

    loosely-coupled and Testable Code Dependency Injection via Method Parameter

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking;
+
+[TestFixture]
+public class VideoServiceTests
+{
+    private VideoService _VideoService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _VideoService = new VideoService();
+    }
+    
+    [Test]
+    public void ReadVideoTitle_EmptyFile_ReturnErrorMsg()
+    {
+        var result = _VideoService.ReadVideoTitle(new FakeFileReader());
+        
+        Assert.That(result, Does.Contain("error").IgnoreCase);
+    }
+}
---
+++ TestNinja/Mocking/Programme.cs
@@ -0,0 +1,10 @@
+﻿namespace TestNinja.Mocking;
+
+public class Programme
+{
+    public static void Main()
+    {
+        var service = new VideoService();
+        var title = service.ReadVideoTitle(new FileReader());
+    }
+}
