15615d1c8cf0213614aa2241099c8e61e8c92e05
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-20 15:07:51 +0530

    mock.Verify() to verify a method is called with valid argument or not

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking;
+
+[TestFixture]
+public class OrderServiceTests
+{
+    private Mock<IStorage> _mockStoage;
+    private OrderService _orderService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockStoage = new Mock<IStorage>();
+        _orderService = new OrderService(_mockStoage.Object);
+    }
+
+    [Test]
+    public void PlaceOrder_WhenCalled_StoreTheOrder()
+    {
+        var order = new Order();
+        _orderService.PlaceOrder(order);
+        
+        _mockStoage.Verify(s => s.Store(order));
+    }
+}
