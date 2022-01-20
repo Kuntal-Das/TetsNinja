8873c03d74ae38eac48551aa770c98d7830e4fa9
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-20 15:29:59 +0530

    How not to abuse mocking

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking;
+
+[TestFixture]
+public class ProductTests
+{
+    [Test]
+    public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+    {
+        var product = new Product() {ListPrice = 100};
+
+        var result = product.GetPrice(new Customer() {IsGold = true});
+        
+        Assert.That(result, Is.EqualTo(70));
+    } 
+    
+    //Abusing Mock ------------ NOT RECOMMENDED
+    //use mock for removing external resources from the UnitTests 
+    [Test]
+    public void GetPrice_GoldCustomer_Apply30PercentDiscount2()
+    {
+        var mockCustomer = new Mock<ICustomer>();
+        mockCustomer.Setup(c => c.IsGold).Returns(true);
+        
+        var product = new Product() {ListPrice = 100};
+
+        var result = product.GetPrice(mockCustomer.Object);
+        
+        Assert.That(result, Is.EqualTo(70));
+    } 
+}
