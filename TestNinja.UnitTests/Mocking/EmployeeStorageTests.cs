6ab6c2b5b862d837540d4510d3f561e1295070b4
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-21 15:22:08 +0530

    Mocking Ex3: Controller testing

---
+++ TestNinja.UnitTests/Mocking/EmployeeStorageTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking;
+
+[TestFixture]
+public class EmployeeStorageTests
+{
+    private EmployeeController _empController;
+    private Mock<IEmployeeStorage> _mockEmpStorage;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockEmpStorage = new Mock<IEmployeeStorage>();
+        _empController = new EmployeeController(_mockEmpStorage.Object);
+    }
+    
+    [Test]
+    public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+    {
+        _empController.DeleteEmployee(1);
+
+        _mockEmpStorage.Verify(s => s.DeleteEmployee(1));
+    }
+
+    [Test]
+    public void DeleteEmployee_WhenCalled_ReturnsRedirectResult()
+    {
+        var result = _empController.DeleteEmployee(1);
+        
+        Assert.That(result, Is.InstanceOf<RedirectResult>());
+    }
+}
