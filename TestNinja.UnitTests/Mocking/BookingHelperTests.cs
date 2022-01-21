d16e0b690e5905a7379b6a0d8805b3c3e2f16a92
Kuntal Das <25ckuntaldas@gmail.com>
2022-01-21 21:26:06 +0530

    Challenge 1: Booking Starts And Finishes Before An Existing Booking

---
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking;
+
+[TestFixture]
+public class BookingHelperTests
+{
+    private Mock<IBookingRepo> _mockBookingRepo;
+    private Booking _existingBooking;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockBookingRepo = new Mock<IBookingRepo>();
+        _existingBooking = new Booking
+        {
+            Id = 2,
+            ArrivalDate = ArriveOn(2022, 1, 1),
+            DepartureDate = DepartOn(2022, 1, 5),
+            Reference = "a"
+        };
+        _mockBookingRepo
+            .Setup(bk => bk.GetActiveBookings(1))
+            .Returns(new List<Booking>() {_existingBooking}.AsQueryable());
+    }
+
+    [Test]
+    public void OverlappingBookingsExist_BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+    {
+        var result = BookingHelper
+            .OverlappingBookingsExist(
+                new Booking()
+                {
+                    Id = 1,
+                    ArrivalDate = Before(_existingBooking.ArrivalDate, days: 2),
+                    DepartureDate = Before(_existingBooking.ArrivalDate),
+                    Status = string.Empty
+                }
+                , _mockBookingRepo.Object
+            );
+
+        Assert.That(result, Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void OverlappingBookingsExist_BookingIsCancelledAndFewOtherBookingFound_ReturnEmptyString()
+    {
+    }
+
+    private DateTime Before(DateTime dateTime, int days = 1)
+    {
+        return dateTime.AddDays(-days);
+    }
+
+    private DateTime After(DateTime dateTime, int days = 1)
+    {
+        return dateTime.AddDays(days);
+    }
+
+    private DateTime ArriveOn(int year, int month, int day)
+    {
+        return new DateTime(year, month, day, 14, 0, 0);
+    }
+
+    private DateTime DepartOn(int year, int month, int day)
+    {
+        return new DateTime(year, month, day, 10, 0, 0);
+    }
+}
---
+++ TestNinja/Mocking/BookingRepo.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace TestNinja.Mocking;
+
+public interface IBookingRepo
+{
+    IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null);
+}
+
+public class BookingRepo : IBookingRepo
+{
+    public IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null)
+    {
+        var unitOfWork = new UnitOfWork();
+        var bookings = unitOfWork
+            .Query<Booking>()
+            .Where(b => b.Status != "Cancelled");
+
+        if (excludedBookingId.HasValue)
+            return bookings.Where(b => b.Id != excludedBookingId.Value);
+        
+        return bookings;
+    }
+}
